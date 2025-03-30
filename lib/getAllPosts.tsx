845d2489de7256802584b9adfb49c7f0eaa4a980
banjo <mr.singledog@gmail.com>
2025-03-30 19:21:55 +0800

    fix errors

--- lib/getAllPosts.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getAllPosts() {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/cruip/cruip-dummy/main/waitlist-posts.json",
-  );
-
-  if (!res.ok) throw new Error("failed to fetch data");
-
-  return res.json();
-}
